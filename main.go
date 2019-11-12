22a34519048e72ee1bc443d49e499b83c9ec2351
goandreus <andreshtml5@gmail.com>
2019-11-12 01:34:43 -0500

    server

--- main.go
+++ main.go
@@ -1,14 +1,43 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"fmt"
+	"github.com/gorilla/mux"
 
-func maing(){
+	"log"
+	"net/http"
+)
+
+func main(){
 	router := mux.NewRouter()
 
 	router.HandleFunc("/signup", signup).Methods("POST")
 	router.HandleFunc("/login", login).Methods("POST")
-	router.HandleFunc("/protected", TokenVerifyMiddleWare(ProtectedEndPoint)).Methods("GET")
+	router.HandleFunc("/protected", TokenVerifyMiddleWare(protectedEndPoint)).Methods("GET")
+
+
+	log.Println("Listen port 3030")
+	log.Fatal(http.ListenAndServe(":3030",router))
 
 
 }
+
+func signup(w http.ResponseWriter, r *http.Request)  {
+ fmt.Println("signup invoked")
+}
+
+func login(w http.ResponseWriter,  r *http.Request)  {
+	fmt.Println("login invoked")
+}
+
+func protectedEndPoint(w http.HandlerFunc,r *http.Request){
+	fmt.Println("protectedEndPoint invoked")
+}
+
+func TokenVerifyMiddleWare(next http.HandlerFunc) http.HandlerFunc{
+	fmt.Println("TokenVerifyMiddleWare")
+	return nil
+}
+
+
 
